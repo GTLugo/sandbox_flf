5d137fd6eff91bd5c4472511707fad53478df554
Gabriel Lugo <g.alexander.lugo@gmail.com>
2022-04-07 11:36:16 -0400

    Changed GameEvent to match new pattern

--- src/test_world.cpp
+++ src/test_world.cpp
@@ -6,56 +6,55 @@
 
 namespace sbx {
   bool TestWorld::onGameEvent(const ff::GameEvent& e) {
-    switch (e.action()) {
-      case ff::GameEvent::Start: {
-        // Set up entities
-        ff::Log::debug("background: {}", background_.id());
-        background_.add<ff::Name>("background")
-            .add<ff::Transform>()
-            .add<ff::Mesh>(
-                ff::VertexArray::create(
-                    { // Vertices
-                        -1.f, -1.f, .1f, /**/ .1, .1, .1, 1.,
-                        1.f, -1.f, .1f, /**/ .1, .1, .1, 1.,
-                        1.f,  1.f, .1f, /**/ .7, .6, .6, 1.,
-                        -1.f,  1.f, .1f, /**/ .7, .6, .6, 1.
-                    },
-                    { // Layout
-                        ff::BufferElement::create<ff::vec3>("pos"),
-                        ff::BufferElement::create<ff::vec4>("color"),
-                    },
-                    {0, 1, 2, 2, 3, 0}), // Indices
-                ff::Shader::create("res/flugel/shaders/simple_shader.glsl")
-            );
-        ff::Log::debug("test_entity: {}", testEntity_.id());
-        testEntity_.add<ff::Name>("test_entity")
-            .add<ff::Transform>()
-            .add<ff::Mesh>(
-                ff::VertexArray::create(
-                    { // Vertices
-                        -.5f, -.5f,  0.f, /**/.7f, .1f, .1f, 1.f,
-                        .5f, -.5f,  0.f, /**/.1f, .7f, .1f, 1.f,
-                        0.f,  .5f,  0.f, /**/.1f, .1f, .7f, 1.f
-                    },
-                    { // Layout
-                        ff::BufferElement::create<ff::vec3>("pos"),
-                        ff::BufferElement::create<ff::vec4>("color"),
-                    },
-                    {0, 1, 2}), // Indices
-                ff::Shader::create("res/flugel/shaders/simple_shader.glsl")
-            );
+    return std::visit(ff::EventVisitor{
+        [=](const ff::GameStartEvent&) {
+          // Set up entities
+          ff::Log::debug("background: {}", background_.id());
+          background_.add<ff::Name>("background")
+              .add<ff::Transform>()
+              .add<ff::Mesh>(
+                  ff::VertexArray::create(
+                      { // Vertices
+                          -1.f, -1.f, .1f, /**/ .1, .1, .1, 1.,
+                          1.f, -1.f, .1f, /**/ .1, .1, .1, 1.,
+                          1.f,  1.f, .1f, /**/ .7, .6, .6, 1.,
+                          -1.f,  1.f, .1f, /**/ .7, .6, .6, 1.
+                      },
+                      { // Layout
+                          ff::BufferElement::create<ff::vec3>("pos"),
+                          ff::BufferElement::create<ff::vec4>("color"),
+                      },
+                      {0, 1, 2, 2, 3, 0}), // Indices
+                  ff::Shader::create("res/flugel/shaders/simple_shader.glsl")
+              );
+          ff::Log::debug("test_entity: {}", testEntity_.id());
+          testEntity_.add<ff::Name>("test_entity")
+              .add<ff::Transform>()
+              .add<ff::Mesh>(
+                  ff::VertexArray::create(
+                      { // Vertices
+                          -.5f, -.5f,  0.f, /**/.7f, .1f, .1f, 1.f,
+                          .5f, -.5f,  0.f, /**/.1f, .7f, .1f, 1.f,
+                          0.f,  .5f,  0.f, /**/.1f, .1f, .7f, 1.f
+                      },
+                      { // Layout
+                          ff::BufferElement::create<ff::vec3>("pos"),
+                          ff::BufferElement::create<ff::vec4>("color"),
+                      },
+                      {0, 1, 2}), // Indices
+                  ff::Shader::create("res/flugel/shaders/simple_shader.glsl")
+              );
 
-        break;
-      }
-      case ff::GameEvent::Stop: {
-        testEntity_.kill();
-        background_.kill();
+          return false;
+        },
+        [=](const ff::GameStopEvent&) {
+          testEntity_.kill();
+          background_.kill();
 
-        break;
-      }
-      default: break;
-    }
-    return false;
+          return false;
+        },
+        [](const auto& event) { return false; }
+    }, e);
   }
 
   bool TestWorld::onInputEvent(const ff::InputEvent& e) {
